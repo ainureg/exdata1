565f26416fdedf2168e3570e1689d277035070ac
ainureg <ainureg@ya.ru>
2015-06-04 12:08:52 +0300

    selected

--- 1.r
+++ 1.r
@@ -8,4 +8,8 @@ temp <- tempfile()
 fileUrl<-"http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl,temp)
 unzip(temp,exdir="./data" )
-data<-read.table("household_power_consumption.txt", colClasses = "character", sep=";")
+data<-read.table("./data/household_power_consumption.txt", colClasses = "character", sep=";", header=TRUE)
+data$Date<-as.Date(data$Date,"%d/%m/%Y") 
+data1<-as.Date("2007-02-01")
+data2<-as.Date("2007-02-02")
+out<-data[(data$Date>=data1 & data$Date<=data2),]
