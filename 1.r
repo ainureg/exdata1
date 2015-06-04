8864328a9040cdd33720730ab77ce566cf3426da
ainureg <ainureg@ya.ru>
2015-06-04 15:17:09 +0300

    +3

--- 1.r
+++ 1.r
@@ -4,12 +4,30 @@ if (!file.exists("data")){
       dir.create("data")
 }
 
-temp <- tempfile()
-fileUrl<-"http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileUrl,temp)
-unzip(temp,exdir="./data" )
+#temp <- tempfile()
+#fileUrl<-"http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#download.file(fileUrl,temp)
+#unzip(temp,exdir="./data" )
 data<-read.table("./data/household_power_consumption.txt", colClasses = "character", sep=";", header=TRUE)
 data$Date<-as.Date(data$Date,"%d/%m/%Y") 
-data1<-as.Date("2007-02-01")
-data2<-as.Date("2007-02-02")
-out<-data[(data$Date>=data1 & data$Date<=data2),]
+date1<-as.Date("2007-02-01")
+date2<-as.Date("2007-02-02")
+out<-data[(data$Date>=date1 & data$Date<=date2),]
+#1
+
+hist(as.numeric(out$Global_active_power), col="red", xlab = "Global Active Power (kilowatts)", main=" Global Active Power" ) 
+
+#2
+colnames1<-colnames(out)
+dates<-out$Date
+times<-out$Time
+x <- paste(dates, times)
+out<-cbind(out, strptime(x, "%Y-%m-%d %H:%M:%S") )
+colnames(out)<-c(colnames1,"DT")
+plot(out$DT, out$Global_active_power, type="l",ylab ="Global Active Power (kilowatts)", xlab = "")
+
+#3
+with(out, plot(DT,Sub_metering_1, type ="l"))
+with(out, points(DT,Sub_metering_2, type ="l", col="red"))
+with(out, points(DT,Sub_metering_3, type ="l", col="blue"))
+legend("topright",col=c("black","red","blue" ),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
