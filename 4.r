e40df535d945bf279e213cca89c86454d1d455c4
ainureg <ainureg@ya.ru>
2015-06-06 19:13:30 +0300

    done

--- 4.r
+++ 4.r
@@ -30,7 +30,7 @@ plot(out$DT, out$Voltage, type="l",ylab ="Voltage", xlab = "datetime")
 with(out, plot(DT,Sub_metering_1, type ="l", ylab= "Energy sub metering", xlab=""))
 with(out, points(DT,Sub_metering_2, type ="l", col="red"))
 with(out, points(DT,Sub_metering_3, type ="l", col="blue"))
-legend(.4,1 ,col=c("black","red","blue" ),pch="-",lty=1, lwd=2, bty="n", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), xjust = 1)
+legend("topright",col=c("black","red","blue" ),pch="-",lty=1, lwd=2, bty="n", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.7)
 
 plot(out$DT, out$Global_reactive_power, type="l",ylab ="Global Reactive Power", xlab = "datetime")
 
